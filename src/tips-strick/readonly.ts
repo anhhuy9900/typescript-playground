28c765d3b6e554203176b0c05740e853b868d4f3
huynguyen <huynguyen@freec.asia>
2024-03-02 11:08:03 +0700

    add tip & tricks cases to practice

---
+++ src/tips-strick/readonly.ts
@@ -0,0 +1,9 @@
+const arr: Readonly<{  name: string, age: number }> = { name: 'Alice', age: 30 };
+
+// Error Cannot assign to name because it is a read-only property.
+// arr.name = 1;
+
+const arrNumber: ReadonlyArray<number> = [1, 2, 3];
+// Error -> Property push does not exist on type readonly number[]
+// arrNumber.push(4);
+
